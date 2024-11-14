bdb0d8059c4c926d139e398c1049da8ec80ee5f9
harshivg <ganwaniharshiv@gmail.com>
2024-11-14 20:30:47 +0530

    star testimonial

--- src/app/(home)/(root)/_components/testimonials.tsx
+++ src/app/(home)/(root)/_components/testimonials.tsx
@@ -60,14 +60,17 @@ export const Testimonials = () => {
                 <div className="flex flex-col md:flex-row md:justify-around">
                     <div className="w-full sm:p-8 md:w-3/5 mr-8 mb-8">
                         <p className="mb-4 md:mb-20">
-                            "I cleared the highly competitive UPSC CAPF written exam on my first attempt with just 3-4 months of focused preparation. Adhyayan's systematic approach transformed my study routine. Their evidence-based techniques like the Feynman method for concept mastery, structured Pomodoro sessions for time management, and active recall practices significantly enhanced my retention. The practical, science-backed methods were instrumental in achieving this milestone. For anyone seeking efficient and proven study strategies, Adhyayan's approach delivers remarkable results." - Jaya Meena
+                            &quot;I cleared the highly competitive UPSC CAPF written exam on my first attempt with just 3-4 months of focused preparation. Adhyayan's systematic approach transformed my study routine. Their evidence-based techniques like the Feynman method for concept mastery, structured Pomodoro sessions for time management, and active recall practices significantly enhanced my retention. The practical, science-backed methods were instrumental in achieving this milestone. For anyone seeking efficient and proven study strategies, Adhyayan's approach delivers remarkable results.&quot; - Jaya Meena
                         </p>
                         <p className="hidden md:block text-2xl">
                             If you&apos;re a learner, this course is for you!
                         </p>
                     </div>
                     <div className="flex justify-center">
-                        <ImageCard image="/adhyayan-logo.svg" />
+                        <ImageCard 
+                            image="/testimonials/jaya-meena.jpeg" 
+                            width={300}
+                        />
                     </div>
                 </div>
             </div>
